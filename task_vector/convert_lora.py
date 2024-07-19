f7c740fdc2b6d47cc558ad0943014093a6fec105
zibojia <38038800+zibojia@users.noreply.github.com>
2024-07-19 21:59:26 +0800

    Add files via upload

---
+++ task_vector/convert_lora.py
@@ -0,0 +1,77 @@
+import re
+import json
+import torch
+from safetensors.torch import load_file
+import argparse
+
+# Create ArgumentParser object
+parser = argparse.ArgumentParser(description='Process some integers.')
+
+# Add arguments
+parser.add_argument('--tensor_path', type=str, default='', help='')
+parser.add_argument('--unet_path', type=str, default='', help='')
+parser.add_argument('--text_encoder_path', type=str, default='', help='')
+parser.add_argument('--vae_path', type=str, default='', help='')
+
+parser.add_argument('--regulation_path', type=str, default='', help='')
+parser.add_argument('--target_prefix', type=str, default='', help='')
+
+# Parse the arguments
+args = parser.parse_args()
+
+tensor_dict = load_file(args.tensor_path)
+
+state_dict = torch.load(args.unet_path, map_location='cpu')
+text_state_dict = torch.load(args.text_encoder_path, map_location='cpu')
+vae_state_dict = torch.load(args.vae_path, map_location='cpu')
+state_dict = {**vae_state_dict, **state_dict, **text_state_dict}
+
+json_list = json.load(open(f"{args.regulation_path}","r"))
+for item in json_list:
+    key = item["start"]
+    value = item["end"]
+
+state_dict2 = {}
+for key in tensor_dict:
+    org_key = key
+    for it in json_list:
+        reg = it["regression"]
+        key2 = it["start"]
+        value = it["end"]
+        key = key.replace(key2, value)
+        if reg:
+            key = re.sub(key2, value, key)
+
+    key = key.replace(".lora_up","")
+    key = key.replace(".lora_down", "")
+
+    if 'lora_up' in org_key:
+        alpha_ = tensor_dict[org_key.split('.lora_up')[0]+'.alpha']
+        lora_down = org_key.split('.lora_up')[0]+'.lora_down.weight'
+        rank = tensor_dict[org_key].shape[1]
+        if len(tensor_dict[org_key].shape) == 4:
+            lora1 = tensor_dict[org_key].float()
+            w1,_,w3,w4 = lora1.shape
+            lora1 = lora1.permute(0,2,3,1).contiguous()
+            lora1 = lora1.view(-1, rank)
+            lora2 = tensor_dict[lora_down].float()
+            _,w2,w3,w4 = lora2.shape
+            lora2 = lora2.view(rank, -1)
+            weight = alpha_*lora1@lora2/rank
+            weight = weight.view(w1,w2,w3,w4)
+        else:
+            lora1 = tensor_dict[org_key].float()
+            lora2 = tensor_dict[lora_down].float()
+            weight = lora1@lora2
+        state_dict2[key] = weight
+
+    if key.endswith('.alpha'):
+        continue
+    if key in state_dict:
+        continue
+    else:
+        print('### The key doesn\'t match!')
+        exit(0)
+
+target_prefix = args.target_prefix
+torch.save(state_dict2, f'./{target_prefix}_delta.pth')
